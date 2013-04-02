b0ce37621aa8315dd4df7b5ae5ea285c64efd6c1
enricostano <ocirne.onats@gmail.com>
2013-04-02 22:01:24 +0200

    Added Order model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402195945) do
+ActiveRecord::Schema.define(:version => 20130402200052) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "orders", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
