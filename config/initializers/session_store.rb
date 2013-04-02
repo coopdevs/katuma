0fb545c4e48839b92da98e29a632e400eceb702e
enricostano <ocirne.onats@gmail.com>
2013-04-02 21:19:46 +0200

    Added Rails

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Katuma::Application.config.session_store :cookie_store, key: '_katuma_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Katuma::Application.config.session_store :active_record_store
