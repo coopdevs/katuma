e3534ab6ed74781d87aeddd4875d8755dfd451b8
enricostano <ocirne.onats@gmail.com>
2013-04-16 20:46:19 +0200

    Created Customer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130409211557) do
+ActiveRecord::Schema.define(:version => 20130416184139) do
+
+  create_table "customers", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "order_lines", :force => true do |t|
     t.decimal  "quantity"
