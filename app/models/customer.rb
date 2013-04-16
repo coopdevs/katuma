e7ed51dae2b6ca78bb090babd4f0a00128d1eb0c
enricostano <ocirne.onats@gmail.com>
2013-04-16 20:55:35 +0200

    Added associations and validation to Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,10 @@
 class Customer < ActiveRecord::Base
   attr_accessible :name
+
+  has_one :profile
+  has_many :users
+  has_many :orders
+  has_and_belongs_to_many :suppliers
+
+  validates :name, :presence => true
 end
