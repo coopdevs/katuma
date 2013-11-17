bb87750f255141e3f9fa7284f21dc53536c57414
enricostano <ocirne.onats@gmail.com>
2013-11-17 20:39:35 +0100

    Added Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Katuma::Application.routes.draw do
       # ToDo: remove this and put bootstrap data
       # in dashboard index HTML
       get :bootstrap, controller: :users, action: :bootstrap
-
+      post :session, controller: :sessions, action: :create
       resources :users, except: [:new, :edit, :index]
       resources :groups, except: [:new, :edit] do
         resources :waiting_users, controller: :waiting_users, only: [:index, :create, :destroy]
