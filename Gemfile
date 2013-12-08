b90255108e3e4c87c9e9e3c525a40bfed47ac652
enricostano <ocirne.onats@gmail.com>
2013-12-08 02:06:41 +0100

    Added Pundit authorization gem

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'pg'
 
 gem 'bcrypt-ruby', '~> 3.0.0'
 
+gem 'pundit'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::API
   # waiting for https://github.com/rails-api/rails-api/issues/81
   include ActionController::StrongParameters
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  # authorization gem
+  include Pundit
 
   def authenticate
     authenticate_or_request_with_http_token do |token, options|
       api_key = ApiKey.where(access_token: token).first
       @current_user = api_key.user if api_key
     end
+  end
+
+  def current_user
+    @current_user
   end
 end
