fd64b1f7f6ad4db474f19df3e20d31e1d4c1127e
enricostano <ocirne.onats@gmail.com>
2014-01-05 16:05:25 +0100

    Added Group policies specs

---
+++ spec/policies/group_policy_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe GroupPolicy do
+  subject { GroupPolicy.new(user, group) }
+
+  let(:group) { FactoryGirl.create :group }
+
+  context 'A visitor' do
+    let(:user) { FactoryGirl.create :user }
+
+    it { should     permit :create }
+    it { should_not permit :show }
+    it { should_not permit :update }
+    it { should_not permit :destroy }
+  end
+
+  context 'A group admin' do
+    let(:user) do
+      FactoryGirl.create :group_admin, group: group
+    end
+
+    it { should permit :create }
+    it { should permit :show }
+    it { should permit :update }
+    it { should permit :destroy }
+  end
+end
---
+++ spec/support/matchers/pundit_matchers.rb
@@ -0,0 +1,15 @@
+# spec/support/matchers/pundit_matchers.rb
+
+RSpec::Matchers.define :permit do |action|
+  match do |policy|
+    policy.public_send("#{action}?")
+  end
+
+  failure_message_for_should do |policy|
+    "#{policy.class} does not permit #{action} on #{policy.record} for #{policy.user.inspect}."
+  end
+
+  failure_message_for_should_not do |policy|
+    "#{policy.class} does not forbid #{action} on #{policy.record} for #{policy.user.inspect}."
+  end
+end
