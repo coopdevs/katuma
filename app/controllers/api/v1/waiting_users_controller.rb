629c7bd0e648b4588bf1f098891db3e2a4f9b801
enricostano <ocirne.onats@gmail.com>
2014-02-08 02:03:33 +0100

    Fixed WaitingUsersController

--- app/controllers/api/v1/waiting_users_controller.rb
+++ app/controllers/api/v1/waiting_users_controller.rb
@@ -2,23 +2,26 @@ module Api
   module V1
     class WaitingUsersController < ApplicationController
 
-      before_action except: :update do
+      before_action do
         @group = Group.find(params[:group_id])
+      end
+      before_action except: :index do
+        @user = User.find(waiting_users_params[:user_id])
       end
 
       def index
-        render json: @group.waiting_users
+        render json: User.with_role :waiting_user, @group
       end
 
       def create
-        if @group.waiting_users << User.find(waiting_users_params[:user_id])
-          render json: @group.waiting_users
+        if @user.add_role :waiting:user, @group
+          render json: User.with_role :waiting_user, @group
         end
       end
 
       def destroy
-        if @group.waiting_users.delete User.find(waiting_users_params[:user_id])
-          render json: @group.waiting_users
+        if @user.remove_role :waiting_user, @group
+          render json: User.with_role :waiting_user, @group
         end
       end
 
