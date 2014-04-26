2bb2d9931cb7bb2a954ed2c4dc1b5932eb5225f1
enricostano <ocirne.onats@gmail.com>
2014-04-26 17:40:15 +0200

    Added some minor improvement

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,11 +2,11 @@ module Api
   module V1
     class SessionsController < ApplicationController
       def create
-        user = User.where("email = ?", params[:email]).first
+        user = User.where(email: params[:email]).first
         if user && user.authenticate(params[:password])
           render json: user.session_api_key, status: 201
         else
-          render json: {}, status: 401
+          render nothing: true, status: 401
         end
       end
     end
