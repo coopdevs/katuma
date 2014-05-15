47f7525f6732c2facfcbb5d62c609554a6835371
enricostano <ocirne.onats@gmail.com>
2014-05-16 01:54:10 +0200

    Removed rails-api gem and bring sessions to life

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   # authorization gem
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :unauthorized_request
 
   private
+
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+      User.find_by(id: session[:current_user_id])
+  end
 
   def unauthorized_request
     render text: "401 Unauthorized", status: :unauthorized
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Katuma::Application.routes.draw do
 
+
   namespace :api do
     namespace :v1 do
       # ToDo: remove this and put bootstrap data
       # in dashboard index HTML
       get :bootstrap, controller: :users, action: :bootstrap
-      post :session, controller: :sessions, action: :create
+      post :sessions, controller: :sessions, action: :create
+      delete :sessions, controller: :sessions, action: :destroy
       resources :users, except: [:new, :edit, :index]
       resources :groups, except: [:new, :edit] do
         resources :waiting_users, controller: :waiting_users, only: [:index, :create, :destroy]
