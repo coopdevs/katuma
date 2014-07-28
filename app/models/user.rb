e7669f30f6af90027e88363923eb4c4d2a7e1050
enricostano <ocirne.onats@gmail.com>
2014-07-28 22:30:40 +0200

    Removed unused ApiKey model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,19 +15,10 @@ class User < ActiveRecord::Base
   has_many :waiting_groups,
     through: :waiting_list_memberships,
     source: :group
-  has_many :api_keys,
-    dependent: :destroy
 
   validates :name,
     presence: true
   validates :email,
     presence: true,
     uniqueness: true
-
-  # Retrieves the API key for the user
-  #
-  # @return [ApiKey]
-  def session_api_key
-    api_keys.first_or_create
-  end
 end
