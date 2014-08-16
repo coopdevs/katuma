e2a3dbde9ec202950c3d569c3335a81a6ecb27ad
enricostano <ocirne.onats@gmail.com>
2014-08-16 02:03:15 +0200

    Added MembershipPolicy

---
+++ app/policies/membership_policy.rb
@@ -0,0 +1,19 @@
+class MembershipPolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      scope
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def create?
+    Membership.where(group: record.group, user: user, role: Membership::ROLES[:admin].any?)
+  end
+
+  def destroy?
+    Membership.where(group: record.group, user: user, role: Membership::ROLES[:admin].any?
+  end
+end
