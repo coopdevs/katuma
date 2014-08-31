b9a7b155cb37e3a76720ab8556833a123f1159b9
enricostano <ocirne.onats@gmail.com>
2014-08-31 22:19:48 +0200

    Added #admins to Group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,17 +4,21 @@ require 'spec_helper'
 describe Group do
 
   describe 'Validations' do
+
     it 'has a valid factory' do
       expect(FactoryGirl.build(:group)).to be_valid
     end
+
     it { should validate_presence_of(:name) }
   end
 
   describe 'Associations' do
+
     it { should have_one(:profile) }
     it { should have_many(:memberships).dependent(:destroy) }
     it { should have_many(:users).through(:memberships) }
-    it { should have_many(:waiters).through(:memberships) }
     it { should have_many(:users_units) }
+    it { should have_many(:admins).through(:memberships) }
+    it { should have_many(:waiters).through(:memberships) }
   end
 end
