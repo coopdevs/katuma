25dfd6c81972d4e24382638baf341cda697a9a41
enricostano <ocirne.onats@gmail.com>
2014-11-11 18:35:59 -0800

    Updated authentication spec helper

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,4 +1,4 @@
 def authenticate_as(user)
-  controller.stub(:authenticate).and_return(true)
-  controller.stub(:current_user).and_return(user)
+  allow(controller).to receive(:authenticate).and_return(true)
+  allow(controller).to receive(:current_user).and_return(user)
 end
