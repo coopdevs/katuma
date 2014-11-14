53fc233486b974496b44e0b1d37cf804a450528c
enricostano <ocirne.onats@gmail.com>
2014-11-13 18:14:12 -0800

    WIP 5

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,11 +1,12 @@
 class InvitationMailer < ActionMailer::Base
+  helper :public_url
   default from: 'info@katuma.org'
 
   def invite_user(invitation)
+    @invitation = invitation
     @user = invitation.user
     @id = invitation.id
     @group = invitation.group
-    @url = url_for(controller: 'api/v1/invitations', action: 'accept', only_path: false, id: @id, email: invitation.email)
 
     mail(to: invitation.email, subject: "#{ @user.name } invited you to join Katuma")
   end
