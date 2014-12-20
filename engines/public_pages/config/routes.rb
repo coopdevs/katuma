f5533cc6f4d7910897010a7a95ee793e1caca855
enricostano <ocirne.onats@gmail.com>
2014-12-20 00:13:57 -0800

    Added OnboardingController to manage sign up

--- engines/public_pages/config/routes.rb
+++ engines/public_pages/config/routes.rb
@@ -4,4 +4,6 @@ PublicPages::Engine.routes.draw do
   get 'login', to: 'session#login'
   get 'logout', to: 'session#logout'
   post 'login_attempt', to: 'session#login_attempt'
+  get 'signup', to: 'onboarding#signup'
+  post 'create_user', to: 'onboarding#create_user'
 end
