5508e07efd400671927ca84810f48c8e50321472
enricostano <ocirne.onats@gmail.com>
2015-01-24 17:03:24 +0100

    Removed placeholder tests

--- engines/public_pages/test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount PublicPages::Engine => "/public_pages"
-end
--- engines/public_pages/test/public_pages_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PublicPagesTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, PublicPages
-  end
-end
