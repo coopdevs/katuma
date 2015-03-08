4a8a9c5dba37c091e99eb2cd66e2b3b130125e53
Andres Gutiérrez <andresgutgon@gmail.com>
2015-03-08 19:42:40 +0100

    Added me endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Katuma::Application.routes.draw do
       post :sessions, controller: :sessions, action: :create
       delete :sessions, controller: :sessions, action: :destroy
       get :account, controller: :users, action: :account
+      get :me, controller: :credentials, action: :me
       resources :memberships
       resources :users, except: [:new, :edit]
       resources :groups, except: [:new, :edit] do
