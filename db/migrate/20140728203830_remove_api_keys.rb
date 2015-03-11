6d55438fad99731c4bb5a39d45e0cdf276884b55
enricostano <ocirne.onats@gmail.com>
2015-03-11 20:45:40 +0100

    Fixed migrations and dependencies

--- db/migrate/20140728203830_remove_api_keys.rb
+++
@@ -1,5 +0,0 @@
-class RemoveApiKeys < ActiveRecord::Migration
-  def change
-    drop_table :api_keys
-  end
-end
--- db/migrate/20140809171029_remove_roles.rb
+++
@@ -1,6 +0,0 @@
-class RemoveRoles < ActiveRecord::Migration
-  def up
-    drop_table :roles
-    drop_table :users_roles
-  end
-end
