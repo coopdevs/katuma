04224ffb622ae083fe1fd551786244a78d2c7ee2
enricostano <ocirne.onats@gmail.com>
2015-07-04 19:12:34 +0200

    Switched to old school rails app :flame:

--- engines/group/config/routes.rb
+++ engines/group/config/routes.rb
@@ -1,5 +1,12 @@
 Group::Engine.routes.draw do
 
+  get '/dashboard', to: 'groups#index'
+  resources :groups, except: [:index, :edit] do
+    member do
+      get 'settings'
+    end
+  end
+
   namespace :api do
     namespace :v1 do
       resources :memberships, except: [:new, :edit]
--- engines/landing/app/controllers/landing/application_controller.rb
+++ engines/landing/app/controllers/landing/application_controller.rb
@@ -1,8 +1,6 @@
 module Landing
   class ApplicationController < ActionController::Base
 
-    include Shared::Controller::Layout
-    include Shared::Controller::Manifests
     include Shared::Controller::Authentication
   end
 end
