33af090621a5ad3cb749bbad93207107361c80ec
Andres Gutierrez <andres@redbooth.com>
2015-07-30 22:31:48 +0200

    Footer logo

--- engines/shared/app/helpers/shared/application_helper.rb
+++ engines/shared/app/helpers/shared/application_helper.rb
@@ -1,4 +1,21 @@
 module Shared
   module ApplicationHelper
+    # default types: success, error, notice
+    def show_flash(*arguments)
+      messages = [:success, :error, :notice] + arguments
+      flash.select do |key, value|
+        messages.include? key
+      end.each do |type, message|
+        @is_error = (type === :error)
+        unless message.blank?
+          haml_tag :div, class: "alert alert-#{type}" do
+            haml_tag :button, type: 'button', class: 'close', data: {dismiss: 'alert'}, 'aria-label' => 'Close' do
+              haml_tag :span, '&times;', aria: {hidden: true}
+            end
+             html_concat sanitize(messages)
+          end
+        end
+      end
+    end
   end
 end
