1cb158a7d4838bd009636761a17ad3daa0eb10f7
Andres Gutierrez <andres@redbooth.com>
2015-07-31 18:20:33 +0200

    Landing page

--- engines/account/app/controllers/account/application_controller.rb
+++ engines/account/app/controllers/account/application_controller.rb
@@ -1,6 +1,5 @@
 module Account
   class ApplicationController < ActionController::Base
-    helper Shared::ApplicationHelper
 
     include Shared::Controller::Layout
     include Shared::Controller::Manifests
--- engines/landing/app/controllers/landing/landing_controller.rb
+++ engines/landing/app/controllers/landing/landing_controller.rb
@@ -1,7 +1,7 @@
 module Landing
   class LandingController < ApplicationController
-
     before_action :redirect_if_logged_in
+    layout 'shared/layouts/base', only: :main
 
     def main
     end
