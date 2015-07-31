9a3ca72ca1f5b6e870c3e0e6284630dccaea101d
Andres Gutierrez <andres@redbooth.com>
2015-07-31 19:09:02 +0200

    Detect user local in their browser

--- engines/landing/app/controllers/landing/application_controller.rb
+++ engines/landing/app/controllers/landing/application_controller.rb
@@ -2,5 +2,7 @@ module Landing
   class ApplicationController < ActionController::Base
 
     include Shared::Controller::Authentication
+    include Shared::Controller::Base
+    layout 'shared/layouts/base'
   end
 end
