213a6c37b1d9756d2cae10c867bf9b58d2190836
Andres Gutierrez <andres@redbooth.com>
2015-07-31 02:26:53 +0200

    Changed favicon image and use flash error helper

--- engines/shared/app/helpers/shared/application_helper.rb
+++ engines/shared/app/helpers/shared/application_helper.rb
@@ -4,15 +4,19 @@ module Shared
     def show_flash(*arguments)
       messages = [:success, :error, :notice] + arguments
       flash.select do |key, value|
-        messages.include? key
+        messages.include? key.parameterize.underscore.to_sym
       end.each do |type, message|
         @is_error = (type === :error)
+        type = type.parameterize.underscore.to_sym
+        type = type === :error ? :danger : type
         unless message.blank?
           haml_tag :div, class: "alert alert-#{type}" do
             haml_tag :button, type: 'button', class: 'close', data: {dismiss: 'alert'}, 'aria-label' => 'Close' do
-              haml_tag :span, '&times;', aria: {hidden: true}
+              haml_tag :span, aria: {hidden: true} do
+                haml_concat '&times;'
+              end
             end
-             html_concat sanitize(messages)
+            haml_concat sanitize(message)
           end
         end
       end
