d5c788bf7d7f90cb4c8c254cd93be91942371fff
enricostano <ocirne.onats@gmail.com>
2015-08-18 20:10:50 +0200

    Added engine assets to pipeline

--- engines/landing/lib/landing/engine.rb
+++ engines/landing/lib/landing/engine.rb
@@ -1,5 +1,9 @@
 module Landing
   class Engine < ::Rails::Engine
     isolate_namespace Landing
+
+    initializer "landing.assets.precompile" do |app|
+      app.config.assets.precompile += %w(application.css)
+    end
   end
 end
--- engines/shared/lib/shared/engine.rb
+++ engines/shared/lib/shared/engine.rb
@@ -1,5 +1,9 @@
 module Shared
   class Engine < ::Rails::Engine
     isolate_namespace Shared
+
+    initializer "shared.assets.precompile" do |app|
+      app.config.assets.precompile += %w(application.css application.js)
+    end
   end
 end
