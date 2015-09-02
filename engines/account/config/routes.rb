aef8197863f9a32ceb7d61b134187d1e88123555
enricostano <ocirne.onats@gmail.com>
2015-09-02 20:14:15 +0200

    Added login API controller

--- engines/account/config/routes.rb
+++ engines/account/config/routes.rb
@@ -1,20 +1,8 @@
 Account::Engine.routes.draw do
-
-  get  'login',  to: 'session#login'
-  post 'login',  to: 'session#login_attempt', as: 'login_attempt'
-  get  'logout', to: 'session#logout'
-  get  'signup', to: 'signup#new'
-  post 'signup', to: 'signup#create'
-  get  'signups/:id/complete', to: 'signup#complete', as: 'complete_signup'
-  post 'signups/:id/complete', to: 'signup#create_user'
-  get  'complete_confirmed', to: 'signup#complete_from_confirmed_email', as: 'complete_from_confirmed_email'
-
   namespace :api do
     namespace :v1 do
       post :signups, controller: :signups, action: :create
-      get :account, controller: :users, action: :account
-      get :me, controller: :credentials, action: :me
-      resources :users, except: [:new, :edit]
+      post :login, controller: :login, action: :login
     end
   end
 end
