fbcd28927956ef314da182233d8b3e87377a05e1
enricostano <ocirne.onats@gmail.com>
2015-09-19 17:46:34 +0200

    API controllers authentication

--- engines/shared/app/controllers/shared/controller/authentication.rb
+++ engines/shared/app/controllers/shared/controller/authentication.rb
@@ -5,7 +5,6 @@ module Shared
       extend ::ActiveSupport::Concern
 
       included do
-        helper_method :redirect_if_logged_in
         helper_method :current_user
       end
 
@@ -13,36 +12,20 @@ module Shared
 
       def current_user
         return @_current_user if defined?(@_current_user)
-        return nil unless session[:current_user_id]
+        return unless request.env['X-katuma-user-id']
 
         @_current_user = nil
         namespace = self.class.name.split('::').first
         klass     = "#{namespace}::User".constantize rescue nil
         klass   ||= Shared::User::Stub
 
-        @_current_user = klass.find_by_id(session[:current_user_id])
+        @_current_user = klass.find_by_id(request.env['X-katuma-user-id'])
       end
 
       # Checks if the request comes from a logged in user
       #
       def authenticate
-        redirect_to '/logout' unless current_user
-      end
-
-      # Checks if the request comes from a logged in user
-      #
-      # TODO: move to API authentication module
-      #
-      def api_authenticate
-        unless current_user
-          render text: "401 Unauthorized", status: :unauthorized
-        end
-      end
-
-      # Redirects to root page if the user is already logged in
-      #
-      def redirect_if_logged_in
-        redirect_to '/dashboard' if current_user
+        render text: '401 - Unauthorized', status: :unauthorized unless current_user
       end
     end
   end
