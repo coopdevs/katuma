401586ed856759568bf64261417d0db41344e6ef
enricostano <ocirne.onats@gmail.com>
2015-09-19 17:49:00 +0200

    Fixed groups

--- engines/group/app/controllers/group/application_controller.rb
+++ engines/group/app/controllers/group/application_controller.rb
@@ -1,9 +1,5 @@
 module Group
   class ApplicationController < ActionController::Base
-
-    include Shared::Controller::Base
-    include Shared::Controller::Layout
-    include Shared::Controller::Manifests
     include Shared::Controller::Authentication
     include Shared::Controller::Authorization
   end
