c49e70c8b59d03c6b1607e5b23b8b89e7d218f35
enricostano <ocirne.onats@gmail.com>
2015-09-19 17:47:57 +0200

    Users creation via API

--- engines/account/config/routes.rb
+++ engines/account/config/routes.rb
@@ -4,6 +4,7 @@ Account::Engine.routes.draw do
       post '/signups', to: 'signups#create'
       get '/signups/:token', to: 'signups#show'
       post :login, controller: :login, action: :login
+      post '/users', to: 'users#create'
     end
   end
 end
