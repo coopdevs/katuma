c2607c33016756d975bad09e6097f89dceefd13d
enricostano <ocirne.onats@gmail.com>
2015-09-19 17:45:15 +0200

    Fixed signup process via API

--- engines/account/app/controllers/account/api/v1/signups_controller.rb
+++ engines/account/app/controllers/account/api/v1/signups_controller.rb
@@ -3,17 +3,35 @@ module Account
     module V1
       class SignupsController < ApplicationController
 
+        # POST /signups
+        #
         def create
           signup = SignupService.new(signup_params[:email]).execute
 
           if signup
             render status: :ok, json: {}
           else
-            render status: :bad_request,
+            render(
+              status: :bad_request,
               json: {
-              model: signup.class.name,
-              errors: signup.errors.full_messages
-            }
+                model: signup.class.name,
+                errors: signup.errors.full_messages
+              }
+            )
+          end
+        end
+
+        # GET /signups/:token
+        #
+        # TODO: better serialized response
+        #
+        def show
+          signup = ::Account::Signup.find_by_token(signup_show_params[:token])
+
+          if signup
+            render status: :ok, json: { email: signup.email }
+          else
+            render status: :not_found, json: {}
           end
         end
 
@@ -21,6 +39,10 @@ module Account
 
         def signup_params
           params.permit(:email)
+        end
+
+        def signup_show_params
+          params.permit(:token)
         end
       end
     end
