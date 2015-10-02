4c212e5159f87c2a0eec59ea0a5fa32d96958729
Andres Gutierrez <andres@redbooth.com>
2015-10-02 20:22:18 +0200

    Sessions in rails :trollface:

--- engines/account/app/controllers/account/api/v1/session_controller.rb
+++ engines/account/app/controllers/account/api/v1/session_controller.rb
@@ -1,17 +1,26 @@
 module Account
   module Api
     module V1
-      class LoginController < ApplicationController
+      class SessionController < ApplicationController
 
         def login
           user = ::Account::User.find_by_email(login_params[:email])
 
           if user && user.authenticate(login_params[:password])
-            render status: :ok, json: { user_id: user.id }
+            session[:current_user_id] = user.id
+            render status: :ok, json: UserSerializer.new(current_user)
           else
             render status: :unauthorized, json: {}
           end
         end
+
+        # Logs out the user
+        #
+        def logout
+          @_current_user = session[:current_user_id] = nil
+
+          render status: :ok, json: {}
+        end        
 
         private
 
--- engines/account/config/routes.rb
+++ engines/account/config/routes.rb
@@ -3,7 +3,9 @@ Account::Engine.routes.draw do
     namespace :v1 do
       post '/signups', to: 'signups#create'
       get '/signups/:token', to: 'signups#show'
-      post :login, controller: :login, action: :login
+      get '/me', to: 'me#show'
+      post :login, controller: :session, action: :login
+      get :logout, controller: :session, action: :logout
       post '/users', to: 'users#create'
     end
   end
