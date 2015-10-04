770257223eae61aa99d5e318c85fed1ba11e2d35
enricostano <ocirne.onats@gmail.com>
2015-10-05 01:57:50 +0200

    Revert "Sessions in rails :trollface:"
    This reverts commit 37319899fa370d8e973064a46e46d63a5a29f4a1.

--- engines/account/config/routes.rb
+++ engines/account/config/routes.rb
@@ -3,9 +3,7 @@ Account::Engine.routes.draw do
     namespace :v1 do
       post '/signups', to: 'signups#create'
       get '/signups/:token', to: 'signups#show'
-      get '/me', to: 'me#show'
-      post :login, controller: :session, action: :login
-      get :logout, controller: :session, action: :logout
+      post :login, controller: :login, action: :login
       post '/users', to: 'users#create'
     end
   end
