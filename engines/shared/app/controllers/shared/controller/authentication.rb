0baf5c29753a32132454389ef943f7ddbeba139e
enricostano <ocirne.onats@gmail.com>
2015-10-05 02:00:22 +0200

    Fixed request headers access

--- engines/shared/app/controllers/shared/controller/authentication.rb
+++ engines/shared/app/controllers/shared/controller/authentication.rb
@@ -12,20 +12,20 @@ module Shared
 
       def current_user
         return @_current_user if defined?(@_current_user)
-        return unless request.headers['X-katuma-user-id']
+        return unless request.env['HTTP_X_KATUMA_USER_ID']
 
         @_current_user = nil
         namespace = self.class.name.split('::').first
         klass     = "#{namespace}::User".constantize rescue nil
         klass   ||= Shared::User::Stub
 
-        @_current_user = klass.find_by_id(request.headers['X-katuma-user-id'])
+        @_current_user = klass.find_by_id(request.env['HTTP_X_KATUMA_USER_ID'])
       end
 
       # Checks if the request comes from a logged in user
       #
       def authenticate
-        render text: '401 - Unauthorized', status: :unauthorized unless current_user
+        render nothing: true, status: :unauthorized unless current_user
       end
     end
   end
