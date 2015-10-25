8ee3f73af1129473d1e2a0aec282a9ea6d4632aa
Andres Gutierrez <andres@redbooth.com>
2015-10-25 20:21:01 +0100

    Validates present of name and last name. And format of username

--- engines/account/app/models/account/user.rb
+++ engines/account/app/models/account/user.rb
@@ -5,8 +5,13 @@ module Account
 
     has_secure_password
 
+    validates :first_name, :last_name, presence: true
     validates :email,    presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
+    validates :username, format: {
+      with: /\A[a-zA-Z0-9_\-]+\Z/,
+      message: 'Solo puede contener letras, numeros, _ y -. Sin espacios ni otros caracteres como: ;, *, ^, $, ...'
+      }
 
     # @return [String]
     def full_name
