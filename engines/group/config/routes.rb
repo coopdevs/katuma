660be23dc2b03344db0cb106d6328bbd8b26ce53
enricostano <ocirne.onats@gmail.com>
2015-10-29 01:09:19 +0100

    Removed unused routes

--- engines/group/config/routes.rb
+++ engines/group/config/routes.rb
@@ -1,12 +1,4 @@
 Group::Engine.routes.draw do
-
-  get '/dashboard', to: 'groups#index'
-  resources :groups, except: [:index, :edit] do
-    member do
-      get 'settings'
-    end
-  end
-
   namespace :api do
     namespace :v1 do
       resources :memberships, except: [:new, :edit]
