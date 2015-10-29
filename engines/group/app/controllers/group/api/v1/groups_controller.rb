b3e04193f14999830df1b267e676080485ad8c71
enricostano <ocirne.onats@gmail.com>
2015-10-29 01:03:45 +0100

    Group creation via API

--- engines/group/app/controllers/group/api/v1/groups_controller.rb
+++ engines/group/app/controllers/group/api/v1/groups_controller.rb
@@ -5,31 +5,42 @@ module Group
 
         before_action :authenticate
         before_action :find_and_authorize_group, only: [:show, :update, :destroy]
+        before_action :initialize_side_effects, only: [:create]
+        after_action :add_side_effects_links, only: [:create]
 
+        # GET api/v1/groups
+        #
         def index
           render json: GroupsSerializer.new(current_user.groups)
         end
 
+        # GET api/v1/groups/:id
+        #
         def show
           render json: GroupSerializer.new(@group)
         end
 
+        # POST api/v1/groups
+        #
         def create
           group = ::Group::Group.new(group_params)
-          group_creation = GroupCreation.new(group, current_user)
-          if group_creation.create
+          group_creator = GroupCreator.new(group, current_user)
+          if group_creator.create
+            @side_effects << group_creator.side_effects
             render json: GroupSerializer.new(group)
           else
             render(
               status: :bad_request,
               json: {
                 model: group.class.name,
-                errors: group.errors.full_messages
+                errors: group.errors
               }
             )
           end
         end
 
+        # PUT api/v1/groups/:id
+        #
         def update
           if @group.update_attributes(group_params)
             render json: GroupSerializer.new(@group)
@@ -38,12 +49,14 @@ module Group
               status: :bad_request,
               json: {
                 model: @group.class.name,
-                errors: @group.errors.full_messages
+                errors: @group.errors
               }
             )
           end
         end
 
+        # DELETE api/v1/groups/:id
+        #
         def destroy
           render json: @group.destroy
         end
@@ -55,9 +68,33 @@ module Group
         end
 
         def find_and_authorize_group
-          @group = ::Group::Group.find(params[:id])
+          @group = ::Group::Group.find_by_id(params[:id])
 
           authorize @group
+        end
+
+        def initialize_side_effects
+          @side_effects = []
+        end
+
+        # TODO: Extract this to lib or gem
+        #
+        def add_side_effects_links
+          return unless @side_effects.any
+
+          links = @side_effects.flatten.map do |side_effect|
+            "<#{api_link(side_effect)}>; rel=\"created\" type=\"#{model_type(side_effect)}\""
+          end.join(', ')
+
+          response.headers['Link'] = links
+        end
+
+        def api_link(side_effect)
+          "#{web_app_url}/api/v1/#{model_type(side_effect).downcase.pluralize}/#{side_effect.id}"
+        end
+
+        def model_type(side_effect)
+          side_effect.class.name
         end
       end
     end
