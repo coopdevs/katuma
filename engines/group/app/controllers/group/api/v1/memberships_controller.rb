33a2cb60e47ecc957a66d0b189f146a49a047df2
enricostano <ocirne.onats@gmail.com>
2015-10-29 18:07:17 +0100

    Fixed membership controller

--- engines/group/app/controllers/group/api/v1/memberships_controller.rb
+++ engines/group/app/controllers/group/api/v1/memberships_controller.rb
@@ -4,44 +4,57 @@ module Group
       class MembershipsController < ApplicationController
 
         before_action :authenticate
-        before_action :find_and_authorize_membership,
-          only: [:show, :update, :destroy]
+        before_action :find_and_authorize_membership, only: [:show, :update, :destroy]
 
+        # GET /api/v1/memberships
+        #
         def index
-          memberships = Membership.where(group_id: current_user.group_ids)
+          memberships = Membership.where(user_id: current_user.id)
 
           render json: MembershipsSerializer.new(memberships)
         end
 
+        # GET /api/v1/memberships/:id
+        #
         def show
           render json: MembershipSerializer.new(@membership)
         end
 
+        # POST /api/v1/memberships
+        #
         def create
           membership = Membership.new(membership_params)
           if membership.save
             render json: MembershipSerializer.new(membership)
           else
-            render status: :bad_request,
+            render(
+              status: :bad_request,
               json: {
-              model: membership.class.name,
-              errors: membership.errors.full_messages
-            }
+                model: membership.class.name,
+                errors: membership.errors
+              }
+            )
           end
         end
 
+        # PUT /api/v1/memberships/:id
+        #
         def update
           if @membership.update_attributes(membership_params)
             render json: MembershipSerializer.new(@membership)
           else
-            render status: :bad_request,
+            render(
+              status: :bad_request,
               json: {
-              model: @membership.class.name,
-              errors: @membership.errors.full_messages
-            }
+                model: @membership.class.name,
+                errors: @membership.errors
+              }
+            )
           end
         end
 
+        # DELETE /api/v1/memberships/:id
+        #
         def destroy
           render json: @membership.destroy
         end
@@ -53,7 +66,7 @@ module Group
         end
 
         def find_and_authorize_membership
-          @membership = Membership.find(params[:id])
+          @membership = Membership.find_by_id(params[:id])
 
           authorize @membership
         end
