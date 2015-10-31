c957ddb1f0098e022db611d247364fde57a05a10
enricostano <ocirne.onats@gmail.com>
2015-10-31 02:40:19 +0100

    Add bulk invitations

--- engines/onboarding/app/controllers/onboarding/api/v1/invitations_controller.rb
+++ engines/onboarding/app/controllers/onboarding/api/v1/invitations_controller.rb
@@ -2,51 +2,104 @@ module Onboarding
   module Api
     module V1
       class InvitationsController < ApplicationController
-
         before_action :authenticate
-        before_action :load_group, only: [:create]
-        before_action :load_invited_user, only: [:create]
+        before_action :load_group, only: [:create, :bulk]
+        before_action :load_invitation, only: [:accept]
 
-        # Creates a new Invitation
+        # Creates invitations in bulk
+        #
+        # POST /api/v1/invitations/bulk
+        #
+        def bulk
+          authorize @group
+
+          emails = bulk_params[:emails].split(',')
+          valids = valid_emails(emails)
+
+          if valids.any?
+            InvitationService.new.bulk_invite!(@group, current_user, valids)
+
+            render nothing: true, status: :accepted
+          else
+            render nothing: true, status: :bad_request
+          end
+        end
+
+        # POST /api/v1/invitations
         #
         def create
-          invitation = Invitation.new({
-            group:        @group,
-            invited_by:   current_user,
-            invited_user: @invited_user
-          })
+          authorize @group
 
-          authorize(invitation)
+          invitation = InvitationService.new.create!(
+            invitation_params[:email],
+            @group,
+            current_user
+          )
 
-          invitation_service = InvitationService.new(invitation)
-          if invitation_service.create
+          if invitation.valid? && invitation.persisted?
             render json: InvitationSerializer.new(invitation)
           else
-            render status: :bad_request,
-              json: {
-                model: invitation.class.name,
-                errors: invitation.errors.full_messages
-              }
+            render(
+              status: :bad_request,
+              json: { errors: invitation.errors.to_json }
+            )
+          end
+        end
+
+        # POST /api/v1/invitations/accept/:token
+        #
+        def accept
+          user = InvitationService.new.accept!(@invitation, accept_params)
+
+          if user.valid? && user.persisted?
+            render json: UserSerializer.new(user)
+          else
+            render(
+              status: :bad_request,
+              json: user.errors.to_json
+            )
           end
         end
 
         private
 
         def invitation_params
+          params.require(:group_id)
+          params.require(:email)
           params.permit(:group_id, :email)
         end
 
+        # :emails is a String containing a comma separated list of email addresses
+        #
+        def bulk_params
+          params.require(:group_id)
+          params.require(:emails)
+          params.permit(:group_id, :emails)
+        end
+
+        def accept_params
+          params.require(:token)
+          params.permit(:token, :username, :first_name, :last_name, :password, :password_confirmation)
+        end
+
         def load_group
-          @group = current_user.groups.find_by_id(invitation_params[:group_id])
+          @group = ::Onboarding::Group.find_by_id(bulk_params[:group_id])
 
           head :not_found unless @group
         end
 
-        def load_invited_user
-          @invited_user = Onboarding::User.find_by_email(invitation_params[:email])
-          @invited_user = Onboarding::User.create_from_email(invitation_params[:email]) unless @invited_user
+        def load_invitation
+          @invitation = Invitation.find_by_token(accept_params[:token])
 
           head :not_found unless @invited_user
+        end
+
+        # Filter only the valid emails
+        #
+        # @param emails [Array<String>]
+        # @return [Array<String>]
+        def valid_emails(emails)
+          emails.select { |email| ::EmailValidator.valid?(email) }
         end
       end
     end
--- engines/onboarding/app/services/onboarding/invitation_service.rb
+++ engines/onboarding/app/services/onboarding/invitation_service.rb
@@ -1,40 +1,71 @@
 module Onboarding
   class InvitationService
-    attr_accessor :invitation, :creator
 
-    # @param invitation [Invitation]
-    def initialize(invitation)
-      @invitation = invitation
+    def initialize
     end
 
-    # Persists the Invitation
-    # and enqueues an InvitationJob
+    # Persists the Invitation and enqueues an InvitationJob
     #
-    # @return [Boolean]
-    def create
-      if @invitation.save
-        send_invitation_email
+    # @param email [String]
+    # @param group [Group]
+    # @param inviting_user [User]
+    # @return [Invitation]
+    def create!(email, group, inviting_user)
+      invitation = load_invitation(email, group, inviting_user)
 
-        true
+      if invitation.save
+        # TODO: move to background job
+        # invitation.update_attribute(sent_at: Time.zone.utc.now)
+        InvitationMailer.invite_user(invitation).deliver
       end
+
+      invitation
+    end
+
+    # Enqueues a job to create a bunch of invitations
+    #
+    # @param group [Group]
+    # @param inviting_user [User]
+    # @param emails [Array<String>]
+    def bulk_invite!(group, inviting_user, emails)
+      # TODO: move to background job
+      emails.each { |email| create!(email, group, inviting_user) }
     end
 
     # Accepts an invitation
     # a new user is created and added to the group which has been invited
-    def accept!
-      add_user_to_group
+    #
+    # @param signup [Account::Signup]
+    # @param options [Hash]
+    # @option options [String] :username
+    # @option options [String] :password
+    # @option options [String] :password_confirmation
+    # @option options [String] :first_name
+    # @option options [String] :last_name
+    # @return [Account::User]
+    def accept!(invitation, options)
+      user = ::Account::User.new(
+        email: signup.email,
+        username: options[:username],
+        password: options[:password],
+        password_confirmation: options[:password_confirmation],
+        first_name: options[:first_name],
+        last_name: options[:last_name]
+      )
 
-      @invitation.destroy
+      ::ActiveRecord::Base.transaction do
+        invitation.destroy if user.save
+      end
+
+      user
     end
 
     private
 
-    # Sends the invitation by email
-    def send_invitation_email
-      InvitationMailer.invite_user(@invitation).deliver
-    end
-
-    def add_user_to_group
+    def load_invitation(email, group, inviting_user)
+      invitation = Invitation.where(group: group, email: email).first
+      invitation ||= Invitation.new(group: group, email: email, invited_by: inviting_user)
+      invitation
     end
   end
 end
