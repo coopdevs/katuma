65d2e8b24c7a44914e6d41d3f005493315da7413
enricostano <ocirne.onats@gmail.com>
2015-11-18 19:15:23 +0100

    Restored users required columns

--- db/schema.rb
+++ db/schema.rb
@@ -147,8 +147,8 @@ ActiveRecord::Schema.define(version: 20150825155715) do
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
     t.string   "username",        null: false
     t.string   "password_digest", null: false
     t.datetime "created_at"
