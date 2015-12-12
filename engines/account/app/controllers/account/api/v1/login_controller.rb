902bbc716fe3b5aa1ec2bbca41f346711f6356e7
Andres Gutierrez <andres@redbooth.com>
2015-12-12 08:59:27 +0100

    Login with email or username

--- engines/account/app/controllers/account/api/v1/login_controller.rb
+++ engines/account/app/controllers/account/api/v1/login_controller.rb
@@ -4,7 +4,8 @@ module Account
       class LoginController < ApplicationController
 
         def login
-          user = ::Account::User.find_by_email(login_params[:email])
+          login = login_params[:email] || login_params[:username]
+          user = ::Account::User.where('username = :login OR email = :login', login: login).first
 
           if user && user.authenticate(login_params[:password])
             render status: :ok, json: UserSerializer.new(user)
@@ -16,7 +17,7 @@ module Account
         private
 
         def login_params
-          params.permit(:email, :password)
+          params.permit(:username, :email, :password)
         end
       end
     end
