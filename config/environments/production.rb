78541fc702fd24545e75b7f71d05b6db2270a6ef
enricostano <ocirne.onats@gmail.com>
2015-12-30 16:06:27 +0100

    Generic SMTP server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,9 +53,9 @@ Katuma::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.mailgun.org',
-    port:                 587,
-    domain:               'katuma.org',
+    address:              ENV['SMTP_ADDRESS'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['SMTP_DOMAIN'],
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
