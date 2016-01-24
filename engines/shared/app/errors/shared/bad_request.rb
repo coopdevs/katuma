6559e129c870e596384ac7a96033358608bd38e4
Pau Pérez <saulopefa@gmail.com>
2016-01-24 20:06:09 +0100

    Add Bad Request class

---
+++ engines/shared/app/errors/shared/bad_request.rb
@@ -0,0 +1,30 @@
+module Shared
+  class BadRequest < StandardError
+    STATUS_CODE = 400
+    NAME = 'Bad Request'.freeze
+
+    def initialize(item = nil)
+      @item = item
+    end
+
+    def message
+      message_hash || 'Malformed request'.freeze
+    end
+
+    def status_code
+      STATUS_CODE
+    end
+
+    def name
+      NAME
+    end
+
+    private
+
+    attr_reader :item
+
+    def message_hash
+      item.try(:errors)
+    end
+  end
+end
---
+++ engines/shared/spec/errors/shared/bad_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe Shared::BadRequest do
+  let(:bad_request) { described_class.new }
+
+  describe '#message' do
+    subject { bad_request.message }
+
+    context 'when no item is provided' do
+      it { is_expected.to eq('Malformed request') }
+    end
+
+    context 'when an item is provided' do
+      class Thing
+        def name; 'Thing'; end
+        def errors
+          { stuff_id: "can't be empty" }
+        end
+      end
+
+      let(:id) { 1 }
+      let(:item) { Thing.new }
+      let(:bad_request) { described_class.new(item) }
+
+      it { is_expected.to eq(stuff_id: "can't be empty") }
+    end
+  end
+
+  describe '#status_code' do
+    subject { bad_request.status_code }
+    it { is_expected.to eq(400) }
+  end
+
+  describe '#name' do
+    subject { bad_request.name }
+    it { is_expected.to eq('Bad Request') }
+  end
+end
