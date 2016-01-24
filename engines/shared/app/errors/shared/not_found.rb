ae96daf0932461d0c88808622b007c272de6a5f1
Pau Pérez <saulopefa@gmail.com>
2016-01-24 15:45:48 +0100

    Add NotFound class

---
+++ engines/shared/app/errors/shared/not_found.rb
@@ -0,0 +1,42 @@
+module Shared
+  class NotFound < StandardError
+    STATUS_CODE = 404
+    NAME = 'Not Found'.freeze
+
+    def initialize(id = nil, item = nil)
+      @id = id
+      @item = item
+    end
+
+    def message
+      message_hash || 'The item could not be found'.freeze
+    end
+
+    def status_code
+      STATUS_CODE
+    end
+
+    def name
+      NAME
+    end
+
+    private
+
+    attr_reader :id, :item
+
+    def message_hash
+      return unless model_name
+      { model_name => item_message }
+    end
+
+    def item_message
+      "#{model_name.capitalize} with id #{id} not found".freeze
+    end
+
+    # TODO: doesn't AR provide this?
+    def model_name
+      return unless item
+      @model_name ||= item.name.demodulize.downcase
+    end
+  end
+end
---
+++ engines/shared/spec/errors/shared/not_found_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe Shared::NotFound do
+  let(:not_found) { described_class.new }
+
+  describe '#message' do
+    subject { not_found.message }
+
+    context 'when no item is provided' do
+      it { is_expected.to eq('The item could not be found') }
+    end
+
+    context 'when an item is provided' do
+      class Thing
+        def name; 'Thing'; end
+      end
+
+      let(:id) { 1 }
+      let(:item) { Thing.new }
+      let(:not_found) { described_class.new(id, item) }
+
+      it { is_expected.to eq('thing' => "Thing with id #{id} not found") }
+    end
+
+    context 'when an item with namespace is provided' do
+      module Namespace
+        class Thing
+          def name; 'Thing'; end
+        end
+      end
+
+      let(:id) { 1 }
+      let(:item) { Namespace::Thing.new }
+      let(:not_found) { described_class.new(id, item) }
+
+      it { is_expected.to eq('thing' => "Thing with id #{id} not found") }
+    end
+  end
+
+  describe '#status_code' do
+    subject { not_found.status_code }
+    it { is_expected.to eq(404) }
+  end
+
+  describe '#name' do
+    subject { not_found.name }
+    it { is_expected.to eq('Not Found') }
+  end
+end
