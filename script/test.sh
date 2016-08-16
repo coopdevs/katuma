9d9783eeadc0e8d1f7d0900e3e3b5e6e67f47828
enricostano <ocirne.onats@gmail.com>
2016-08-16 19:43:14 +0200

    Better test scripts

--- script/test.sh
+++ script/test.sh
@@ -2,11 +2,6 @@
 
 result=0
 
-bundle install --jobs=3 --retry=3 --deployment
-
-bundle exec rake db:create db:migrate
-RAILS_ENV=test bundle exec rake db:drop db:create db:migrate
-
 for engine_name in $(ls -d engines/*/ | cut -f 2 -d '/'); do
   pushd engines/$engine_name > /dev/null
   ./script/test.sh
