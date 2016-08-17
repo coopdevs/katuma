c1de2fb99afad058afa7e427691d532e73735309
enricostano <ocirne.onats@gmail.com>
2016-08-17 19:38:03 +0200

    Remove bundle install deployment option

--- engines/group/script/test.sh
+++ engines/group/script/test.sh
@@ -5,7 +5,7 @@ engine_name=`pwd | cut -f6 -d '/'`
 
 echo "Running tests for $engine_name engine..."
 
-bundle install --jobs=3 --retry=3 --deployment | grep Installing
+bundle install --jobs=3 --retry=3 | grep Installing
 
 RAILS_ENV=test bundle exec rake db:drop db:create
 RAILS_ENV=test bundle exec rake db:migrate
