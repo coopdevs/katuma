30430ec9a339b6dde2b3b47ef440cebae9909da2
enricostano <ocirne.onats@gmail.com>
2016-09-07 00:30:19 +0200

    Add memberships has_many Producer association

--- engines/producers/app/models/producers/producer.rb
+++ engines/producers/app/models/producers/producer.rb
@@ -3,6 +3,7 @@ module Producers
     self.table_name = :producers
 
     has_many :products
+    has_many :memberships, dependent: :destroy
 
     validates :name, :email, :address, presence: true
   end
