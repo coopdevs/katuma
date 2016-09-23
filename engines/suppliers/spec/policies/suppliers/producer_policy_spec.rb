ce4acc1abcf4368852f1501db86463c9bf31af05
enricostano <ocirne.onats@gmail.com>
2016-09-23 20:52:00 +0200

    Add specs for producers policy in supplier engine

--- engines/suppliers/spec/policies/suppliers/producer_policy_spec.rb
+++ engines/suppliers/spec/policies/suppliers/producer_policy_spec.rb
@@ -11,9 +11,45 @@ module Suppliers
       end
     end
 
-    permissions :show?, :update?, :destroy? do
+    permissions :update?, :destroy? do
       it 'is set to `false` by default' do
         expect(subject).to_not permit(instance_double(User), instance_double(Producer))
+      end
+    end
+
+    permissions :show? do
+      let(:user) do
+        user = FactoryGirl.create(:user)
+        User.find(user.id)
+      end
+      let(:producer) do
+        producer = FactoryGirl.create(:producer)
+        Producer.find(producer.id)
+      end
+
+      context 'when the user pertains to a group associated to the producer' do
+        let(:group) { FactoryGirl.create(:group) }
+        let(:group_user) { ::Group::User.find(user.id) }
+        let(:supplier_group) { ::Suppliers::Group.find(group.id) }
+
+        before do
+          ::Group::Membership.create(
+            group: group,
+            user: group_user,
+            role: Membership::ROLES[:member]
+          )
+          Supplier.create(group: supplier_group, producer: producer)
+        end
+
+        it 'grants access' do
+          expect(subject).to permit(user, producer)
+        end
+      end
+
+      context 'when the user does not pertain to a group associated to the producer' do
+        it 'denies access' do
+          expect(subject).to_not permit(user, producer)
+        end
       end
     end
   end
