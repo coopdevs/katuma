ee80e9c7b616d247ae8dc0e8a9a9f27cd28e633a
enricostano <ocirne.onats@gmail.com>
2016-09-29 22:35:52 +0200

    Fix ProductsController and add specs

--- engines/producers/app/controllers/producers/api/v1/products_controller.rb
+++ engines/producers/app/controllers/producers/api/v1/products_controller.rb
@@ -2,18 +2,18 @@ module Producers
   module Api
     module V1
       class ProductsController < ApplicationController
+        before_action :authenticate
         before_action :load_product, only: [:show, :update, :destroy]
         before_action :load_producer, only: [:index, :create]
 
-        # GET /api/v1/products
+        # GET /api/v1/products?producer_id=
         #
         # You must pass `producer_id` param as filter
         #
         def index
+          products = ::Producers::ProductFinder.new.find_by_producer(@producer)
 
-          products = ProductsFinder.find_by_producer(@producer)
-
-          render json: ProductsSerializer.new(products)
+          render json: ::Producers::ProductsSerializer.new(products)
         end
 
         # GET /api/v1/products/:id
@@ -41,9 +41,12 @@ module Producers
         #
         def update
           if @product.update_attributes(product_params)
-            render :show
+            render json: ProductSerializer.new(@product)
           else
-            render :show, status: :bad_request
+            render(
+              status: :bad_request,
+              json: @product.errors.to_json
+            )
           end
         end
 
@@ -63,20 +66,20 @@ module Producers
           params.permit(:name, :price, :unit, :producer_id)
         end
 
-        def products_params
-          params.permit(:producer_id)
-        end
-
         def load_product
           @product = Product.find_by_id(params[:id])
+
+          return head :not_found unless @product
+
+          authorize @product
         end
 
         def load_producer
-          @producer = Producer.find_by_id(products_params[:producer_id])
+          @producer = Producer.find_by_id(product_params[:producer_id])
 
           return head :not_found unless @producer
 
-          ::Pundit.authorize(current_user, @producer, :show?)
+          authorize @producer
         end
       end
     end
