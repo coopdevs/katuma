88841744c99ec49cd2371d7dfa09de08d4455c6c
enricostano <ocirne.onats@gmail.com>
2016-10-03 19:17:44 +0200

    Add producers routes

--- engines/basic_resources/config/routes.rb
+++ engines/basic_resources/config/routes.rb
@@ -3,6 +3,7 @@ BasicResources::Engine.routes.draw do
     namespace :v1 do
       resources :memberships, except: [:new, :edit]
       resources :groups, except: [:new, :edit]
+      resources :producers, only: [:create, :update, :destroy]
     end
   end
 end
