928940cb1d32c9db4c3633e187e23eca159a3756
enricostano <ocirne.onats@gmail.com>
2016-10-04 20:06:49 +0200

    Fix User associations

--- engines/basic_resources/app/models/basic_resources/user.rb
+++ engines/basic_resources/app/models/basic_resources/user.rb
@@ -6,6 +6,7 @@ module BasicResources
     include Shared::Model::ReadOnly
 
     has_many :memberships, dependent: :destroy
-    has_many :groups, through: :memberships
+    has_many :groups, through: :memberships, source: :basic_resource_group
+    has_many :producers, through: :memberships, source: :basic_resource_producer
   end
 end
--- engines/basic_resources/spec/models/basic_rersources/user_spec.rb
+++ engines/basic_resources/spec/models/basic_rersources/user_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 module BasicResources
   describe User do
     describe 'Associations' do
-      it { should have_many(:memberships).dependent(:destroy) }
-      it { should have_many(:groups).through(:memberships) }
+      it { is_expected.to have_many(:memberships).dependent(:destroy) }
+      it { is_expected.to have_many(:groups).through(:memberships) }
+      it { is_expected.to have_many(:producers).through(:memberships) }
     end
   end
 end
