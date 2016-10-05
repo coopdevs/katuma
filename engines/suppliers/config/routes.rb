9c6b7e7da25cac9352f9894e9cfcd6db47c35e84
enricostano <ocirne.onats@gmail.com>
2016-10-05 19:03:42 +0200

    Fix Suppliers routes

--- engines/suppliers/config/routes.rb
+++ engines/suppliers/config/routes.rb
@@ -2,7 +2,7 @@ Suppliers::Engine.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :suppliers, except: [:new, :edit]
-      resources :providers, only: [:index, :show]
+      resources :producers, only: [:index, :show]
     end
   end
 end
