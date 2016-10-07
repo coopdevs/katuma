960417eb7b565f0a7dbc5f8d1f814df18ae4bb14
enricostano <ocirne.onats@gmail.com>
2016-10-07 19:07:30 +0200

    Add associations and #has_admin? to Producer in Suppliers engine

--- engines/suppliers/app/models/suppliers/producer.rb
+++ engines/suppliers/app/models/suppliers/producer.rb
@@ -3,6 +3,17 @@ module Suppliers
     include Shared::Model::ReadOnly
 
     self.table_name = :producers
+
+    has_many :memberships, foreign_key: :basic_resource_producer_id
+    # TODO: investigate how to scope memberships.role = $admin here
+    has_many :groups, through: :memberships
+
+    # @return [Boolean]
+    def has_admin?(user)
+      memberships.where(
+        user_id: user.id,
+        role: Membership::ROLES[:admin]
+      ).any?
+    end
   end
 end
-
