63d78b17717a6eeecca3c64df782df88219bcdc1
enricostano <ocirne.onats@gmail.com>
2016-10-07 18:59:09 +0200

    Add associations to User in Suppliers engine

---
+++ engines/suppliers/spec/models/suppliers/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+module Suppliers
+  describe User do
+    describe 'Associations' do
+      it { is_expected.to have_many(:memberships) }
+      it { is_expected.to have_many(:producers).through(:memberships).source(:basic_resource_producer) }
+    end
+  end
+end
