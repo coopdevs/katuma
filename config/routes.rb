5fdf727fe90adc73e8bc372d6d93e86caec40a0d
enricostano <ocirne.onats@gmail.com>
2016-10-07 20:19:53 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Katuma::Application.routes.draw do
   mount Account::Engine,        at: '/'
   mount BasicResources::Engine, at: '/'
   mount Onboarding::Engine,     at: '/'
-  mount Producers::Engine,      at: '/'
+  mount Products::Engine,       at: '/'
   mount Suppliers::Engine,      at: '/'
 end
