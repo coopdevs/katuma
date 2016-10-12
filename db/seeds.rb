355fdbb34d953c0f306c62a64b870068f2c97234
enricostano <ocirne.onats@gmail.com>
2016-10-12 15:36:44 +0200

    Add moar awesomeness to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-user = ::Account::User.create(
+# Users
+
+joanin = ::Account::User.create!(
   email: 'joanin@katuma.org',
   first_name: 'Joanin',
   last_name: 'Cerea',
@@ -6,12 +8,66 @@ user = ::Account::User.create(
   password: 'kakaka22'
 )
 
-group = ::BasicResources::Group.create(
+frida = ::Account::User.create!(
+  email: 'frida@katuma.org',
+  first_name: 'Frida',
+  last_name: 'Fola',
+  username: 'frida',
+  password: 'kakaka22'
+)
+
+# Groups
+
+tomatika = ::BasicResources::Group.create!(
   name: 'Tomatika'
 )
 
-::BasicResources::Membership.create(
-  user_id: user.id,
-  basic_resource_group_id: group.id,
+::BasicResources::Membership.create!(
+  user_id: joanin.id,
+  basic_resource_group_id: tomatika.id,
   role: ::BasicResources::Membership::ROLES[:admin]
 )
+
+::BasicResources::Membership.create!(
+  user_id: frida.id,
+  basic_resource_group_id: tomatika.id,
+  role: ::BasicResources::Membership::ROLES[:member]
+)
+
+cabas = ::BasicResources::Group.create!(
+  name: 'Cabas'
+)
+
+::BasicResources::Membership.create!(
+  user_id: frida.id,
+  basic_resource_group_id: cabas.id,
+  role: ::BasicResources::Membership::ROLES[:admin]
+)
+
+# Producers
+
+jaume = ::BasicResources::Producer.new(
+  name: 'El Jaume',
+  email: 'jaume@eljaume.coop',
+  address: 'c/ de les peras, 33, Barbera del Valles'
+)
+::BasicResources::ProducerCreator.new(
+  producer: jaume,
+  creator: joanin,
+  group: tomatika
+).create!
+::Suppliers::Supplier.create!(
+  group_id: tomatika.id,
+  producer_id: jaume.id
+)
+
+prueba = ::BasicResources::Producer.new(
+  name: 'Productor de prueba',
+  email: 'test@katuma.org',
+  address: 'c/ de las pruebas, Barcelona'
+)
+::BasicResources::ProducerCreator.new(
+  producer: prueba,
+  creator: joanin,
+  group: tomatika
+).create!
